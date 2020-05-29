66a114d3e644b1d8eee31cb24d426fc5d48ea816
Benjamin Friske <benjaminfriske@gmail.com>
2020-05-29 14:49:02 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Learning.DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

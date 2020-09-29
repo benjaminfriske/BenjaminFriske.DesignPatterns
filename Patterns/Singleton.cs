c54279aacb6db0aa2d53c482d478b8c288bc0c50
benjaminfriske <benjaminfriske@gmail.com>
2020-09-29 08:07:56 -0500

    Added stylecop, and singleton implementation.

---
+++ Patterns/Singleton.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="Singleton.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Learning.DesignPatterns.Console.Patterns
+{
+#nullable enable
+    /// <summary>
+    /// Singleton summary.
+    /// </summary>
+    public sealed class Singleton
+    {
+        private static Singleton? instance;
+
+        private Singleton()
+        {
+        }
+
+        /// <summary>
+        /// Gets the instance of singleton class.
+        /// </summary>
+        public static Singleton Instance
+        {
+            get
+            {
+                return instance ??= new Singleton();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 ﻿using Learning.DesignPatterns.Domain.Models;
-using System;
 
 namespace Learning.DesignPatterns.Console
 {
-    class Program
+    /// <summary>
+    /// Program to study design patterns.
+    /// </summary>
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Customer customer = new Customer();
             customer.Name = "Benjamin Friske";

9f4e395e36784a2d0ee583df9d7777308f45a803
benjaminfriske <benjaminfriske@gmail.com>
2020-09-29 16:10:44 -0500

    Added tests directory and project.

---
+++ Tests/Patterns/SingletonTests.cs
@@ -0,0 +1,25 @@
+﻿using Learning.DesignPatterns.Console.Patterns;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Learning.DesignPatterns.Console.Tests.Patterns
+{
+    [TestClass]
+    public class SingletonTests
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_ReturnsNonNullSingletonInstance_ValidSingletonClass()
+        {
+            //Arrange
+            //Act 
+            //Assert
+            Assert.IsNotNull(Singleton.Instance);
+        }
+    }
+}

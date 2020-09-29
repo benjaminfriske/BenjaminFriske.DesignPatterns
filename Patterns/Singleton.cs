3705281314a5069225ec0ac78fc59f3ce509b017
benjaminfriske <benjaminfriske@gmail.com>
2020-09-29 18:54:27 -0500

    Singleton approaches and stylecop suppression.

--- Patterns/Singleton.cs
+++ Patterns/Singleton.cs
@@ -2,17 +2,20 @@
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
+using System;
+
 namespace Learning.DesignPatterns.Console.Patterns
 {
 #nullable enable
+
     /// <summary>
-    /// Singleton summary.
+    /// Singleton lock approach.
     /// </summary>
     public sealed class Singleton
     {
-        private static Singleton? instance;
+        private static readonly Singleton instance = new Singleton();
 
-        private Singleton()
+        static Singleton()
         {
         }
 
@@ -23,7 +26,76 @@ namespace Learning.DesignPatterns.Console.Patterns
         {
             get
             {
-                return instance ??= new Singleton();
+                return Nested.Instance;
+            }
+        }
+
+        private class Nested
+        {
+            internal static readonly Singleton Instance = new Singleton();
+
+            static Nested()
+            {
+            }
+        }
+    }
+
+    /// <summary>
+    /// Singleton lazy approach.
+    /// </summary>
+    public sealed class SingletonLazy
+    {
+        /// <summary>
+        /// This is document.
+        /// </summary>
+        public static readonly Lazy<SingletonLazy> instance = new Lazy<SingletonLazy>(() => new SingletonLazy());
+
+        private SingletonLazy()
+        {
+        }
+
+        /// <summary>
+        /// Gets the instance of singleton class.
+        /// </summary>
+        public static SingletonLazy Instance
+        {
+            get
+            {
+                return instance.Value;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Singleton lock approach.
+    /// </summary>
+    public sealed class SingletonLock
+    {
+        private static readonly object PadLock = new object();
+        private static SingletonLock? instance;
+
+        private SingletonLock()
+        {
+        }
+
+        /// <summary>
+        /// Gets the instance of singleton class.
+        /// </summary>
+        public static SingletonLock Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (PadLock)
+                    {
+                        return instance ??= new SingletonLock();
+                    }
+                }
+                else
+                {
+                    return instance;
+                }
             }
         }
     }
